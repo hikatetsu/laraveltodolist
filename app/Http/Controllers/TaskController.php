37e6032e7686381238aa8d5588f19a0ef5123d78
tetsu <cda07c5117@gmail.com>
2020-12-28 23:26:17 +0900

    エラーハンドリング

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -71,6 +71,8 @@ class TaskController extends Controller
      */
     public function showEditForm(Folder $folder, Task $task)
     {
+        $this->checkRelation($folder, $task);
+
         return view('tasks/edit',[
             'task' => $task,
         ]);
@@ -85,6 +87,8 @@ class TaskController extends Controller
      */
     public function edit(Folder $folder,  Task $task, EditTask $request)
     {
+        $this->checkRelation($folder, $task);
+
         $task->title = $request->title;
         $task->status = $request->status;
         $task->due_date = $request->due_date;
@@ -93,5 +97,12 @@ class TaskController extends Controller
         return redirect()->route('tasks.index',[
             'folder' => $task->folder_id,
         ]);
+    }
+
+    private function checkRelation(Folder $folder, Task $task)
+    {
+        if ($folder->id !== $task->folder_id) {
+            abort(404);
+        }
     }
 }
